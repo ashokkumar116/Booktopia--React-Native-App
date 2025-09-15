3a88ab3275afe764c2bf3e3404be0f6a33cd1d71
ASHOK KUMAR P <ashokkumarpandian7@gmail.com>
2025-09-15 20:45:32 +0530

    Created Book Schema

---
+++ Server/Models/Book.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const bookSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    rating:{
+        type:Number,
+        min:0,
+        max:5,
+        required:true,
+    },
+    caption:{
+        type:String,
+        required:true,
+    },
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true,
+    }
+},{
+    timestamps:true
+})
+
+const Book = mongoose.model('Book',bookSchema);
+
+module.exports = Book;
