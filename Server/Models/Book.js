65af2f904e1ff6a6d6936a431692fa7d84c2bc7d
ASHOK KUMAR P <ashokkumarpandian7@gmail.com>
2025-09-15 22:19:19 +0530

    Modified Book Schema

--- Server/Models/Book.js
+++ Server/Models/Book.js
@@ -15,6 +15,10 @@ const bookSchema = new mongoose.Schema({
         type:String,
         required:true,
     },
+    image:{
+        type:String,
+        default:"",
+    },
     user:{
         type:mongoose.Schema.Types.ObjectId,
         ref:'User',
