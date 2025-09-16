b9308ed7f6f11e2894dff39f78cf3b4d92a5405d
ASHOK KUMAR P <ashokkumarpandian7@gmail.com>
2025-09-16 21:03:59 +0530

    Add Pagination to GetAllBooks API

--- Server/Controllers/bookControllers.js
+++ Server/Controllers/bookControllers.js
@@ -1,30 +1,29 @@
 const Book = require("../Models/Book");
-const cloudinary = require('../Services/cloudinary')
+const cloudinary = require("../Services/cloudinary");
 
 const addBook = async (req, res) => {
-    const { title, rating, caption,image } = req.body;
+    const { title, rating, caption, image } = req.body;
     const user = req.user;
 
     if (!user) {
         return res.status(401).json({ message: "Unauthorized" });
     }
-    
+
     if (!title || !rating || !caption || !image) {
         return res.status(400).json({ message: "All fields are required" });
     }
 
-    const imageResponse = await cloudinary.uploader.upload(image,{
-        folder:"BookTopia"
-    })
-    
-    const imageUrl = imageResponse.secure_url;
+    const imageResponse = await cloudinary.uploader.upload(image, {
+        folder: "BookTopia",
+    });
 
+    const imageUrl = imageResponse.secure_url;
 
     const book = new Book({
         title,
         rating,
         caption,
-        image:imageUrl,
+        image: imageUrl,
         user: user._id,
     });
 
@@ -35,32 +34,46 @@ const addBook = async (req, res) => {
 
 const getAllBooks = async (req, res) => {
     const user = req.user;
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 5;
+    const skip = (page - 1) * limit;
 
     if (!user) {
         return res.status(401).json({ message: "Unauthorized" });
     }
 
-    const books = await Book.find().populate("user", "username profileImage");
+    const totalBooks = await Book.countDocuments();
+    const totalPages = Math.ceil(totalBooks / limit);
+
+    const books = await Book.find()
+        .sort({ createdAt: -1 })
+        .skip(skip)
+        .limit(limit)
+        .populate("user", "username profileImage");
+
+    return res.status(200).json({
+        books,
+        totalBooks,
+        totalPages,
+        currentPage:page,
+    });
+};
+
+const getMyBooks = async (req, res) => {
+    const userId = req.user._id;
+
+    const books = await Book.find({ user: userId }).populate(
+        "user",
+        "username profileImage"
+    );
 
     return res.status(200).json({
         books,
     });
 };
 
-
-const getMyBooks = async(req,res)=>{
-    const userId = req.user._id;
-
-    const books = await Book.find({user:userId}).populate('user',"username profileImage");
-
-    return res.status(200).json({
-        books
-    })
-
-}
-
 module.exports = {
     addBook,
     getAllBooks,
-    getMyBooks
+    getMyBooks,
 };
